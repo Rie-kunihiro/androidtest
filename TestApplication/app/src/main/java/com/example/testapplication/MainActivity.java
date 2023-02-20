6710246e0b5b836513185c64270e0de167d445dd
Rie <hasegawa001@gannon.edu>
2023-02-20 14:32:50 -0500

    Activity lifecicle changes

--- TestApplication/app/src/main/java/com/example/testapplication/MainActivity.java
+++ TestApplication/app/src/main/java/com/example/testapplication/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.testapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ActionMode;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e(TAG, "onCreate: MainActivity");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "onStart(): MainActivity");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume(): MainActivity");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause(): MainActivity");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop(): MainActivity");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(TAG, "onRestart(): MainActivity");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy(): MainActivity");
+    }
+
+    public void launchSecondActivity(View view) {
+        Intent inten = new Intent(this, MainActivity2.class);
+        startActivity(inten);
     }
 }
---
+++ TestApplication/app/src/main/java/com/example/testapplication/MainActivity2.java
@@ -0,0 +1,46 @@
+package com.example.testapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Log.e(TAG, "onCreate(): SecondActivity");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause(): SecondActivity");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop(): SecondActivity");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume(): SecondActivity");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(TAG, "onRestart(): SecondActivity");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "onDestroy(): SecondActivity");
+    }
+}
