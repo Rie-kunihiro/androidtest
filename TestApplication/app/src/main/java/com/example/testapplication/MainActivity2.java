3dbd05d0b98e603b96f18d51eba67fa03451d24f
Rie <hasegawa001@gannon.edu>
2023-02-21 14:57:58 -0500

    service

--- TestApplication/app/src/main/java/com/example/testapplication/MainActivity2.java
+++ TestApplication/app/src/main/java/com/example/testapplication/MainActivity2.java
@@ -7,6 +7,8 @@ import android.util.Log;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    public static final String TAG = MainActivity2.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
